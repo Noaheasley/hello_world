2f6977527271035e0193f24bb55e936f0f7bf6bc
Noah Easley <noaheasley408@gmail.com>
2020-08-17 16:25:06 -0500

    player stats(health and level)

--- Game.cs
+++ Game.cs
@@ -8,7 +8,15 @@ namespace HelloWorld
     {
         public void Run()
         {
-
+            int maxLevel = 100;
+            int level = maxLevel;
+            maxLevel = 120;
+            string name = Console.ReadLine();
+            float health = 100.0f;
+            float healthRegen = 20;
+            bool ready = false;
+            health = health + healthRegen;
+            Console.WriteLine(name + " you live :D " + healthRegen);
         }
     }
 }
